dbffb208d4d5b00632eb92228930cc70f608b753
Darker_Than_Black <htunlukhant@gmail.com>
2019-08-06 15:22:12 +0630

    Initial Commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by tunlukhant on 8/6/19.
+//  Copyright © 2019 tunlukhant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
